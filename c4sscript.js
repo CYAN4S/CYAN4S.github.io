ae3e461f2c606a821c47fd7eed88dc3353e99dfb
CYAN4S <37798132+CYAN4S@users.noreply.github.com>
2019-10-01 14:47:59 +0900

    Temporary

--- c4sscript.js
+++
@@ -1,9 +0,0 @@
-function hideNav() {
-    let nav = document.getElementById("fullnav");
-    nav.style.display = "none";
-}
-
-function showNav() {
-    let nav = document.getElementById("fullnav");
-    nav.style.display = "block";
-}
