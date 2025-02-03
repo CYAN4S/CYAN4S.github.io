adc6fb69c944136b2151ab8a5f178e1d685e1ac2
CYAN4S <me@cyan4s.com>
2025-02-04 02:53:37 +0900

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cyan4s.com",
-  integrations: [mdx(), sitemap(), solid()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
